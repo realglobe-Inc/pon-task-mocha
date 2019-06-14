9f8a847f7ff60fcc3dd1b7c5070bb46d1915e97a
okunishinishi <okunishinishi@gmail.com>
2019-06-14 11:33:34 +0900

    テストが失敗したら例外を投げる
    https://github.com/realglobe-Inc/hec-eye/issues/1103 より

---
+++ misc/mocks/mock-test-to-fail.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const { ok } = require('assert')
+
+describe('foo-fail', () => {
+  it('bar-fail', () => {
+    ok(false)
+  })
+})
--- test/define_test.js
+++ test/define_test.js
@@ -6,7 +6,7 @@
 
 const define = require('../lib/define.js')
 const ponContext = require('pon-context')
-const {ok} = require('assert')
+const { strict: { ok, equal } } = require('assert')
 
 describe('define', function () {
   this.timeout(3000)
@@ -17,6 +17,16 @@ describe('define', function () {
     ok(task)
 
     await Promise.resolve(task(ctx))
+  })
+
+  it('Test to fail', async () => {
+    const ctx = ponContext()
+    const task = define(`${__dirname}/../misc/mocks/*-test-to-fail.js`, {})
+    ok(task)
+    const caught = await Promise.resolve(task(ctx))
+      .then(() => null)
+      .catch((e) => e)
+    ok(caught)
   })
 })
 
